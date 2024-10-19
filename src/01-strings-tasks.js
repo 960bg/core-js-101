c2aaed44a2330e002e9302a9d35f2646e714fcb2
960bg <74264055+960bg@users.noreply.github.com>
2024-10-19 12:27:55 +0400

    feat: done string task

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -335,8 +335,11 @@ function encodeToRot13(str) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(v) {
+  if (typeof v === 'object' && v !== null) {
+    return (typeof v.valueOf() === 'string');
+  }
+  return (typeof v === 'string');
 }
 
 
@@ -364,8 +367,14 @@ function isString(/* value */) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  const mas = [
+    'A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣',
+    'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦',
+    'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥',
+    'A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠',
+  ];
+  return mas.indexOf(value);
 }
 
 
